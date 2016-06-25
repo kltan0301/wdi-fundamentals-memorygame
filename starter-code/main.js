78052e5a150e6e482a0cf1e94be3ec607b35655e
Kai Lin Tan <kailin.tan.2010@gmail.com>
2016-06-25 14:40:40 +0800

    Created board logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -8,3 +8,15 @@ if(cardOne == cardTwo){
 }else {
 	alert("Sorry, try again.");
 }
+
+function createBoard(count){
+
+	var board = document.getElementById('game-board'); 
+	
+	for(var i = 0; i < count; i++){
+		var card = document.createElement('div');
+		card.setAttribute("class", "card"); 
+		board.appendChild(card);
+	}
+}
+createBoard(4);
