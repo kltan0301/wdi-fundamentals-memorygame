7f697d4a3c3d416c8b57bddbfa238eabdccfce55
Kai Lin Tan <kailin.tan.2010@gmail.com>
2016-06-26 17:10:11 +0800

    Updated game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,22 +1,51 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-if(cardOne == cardTwo){
-	alert("You found a match!");
-}else {
-	alert("Sorry, try again.");
-}
-
-function createBoard(count){
+function createBoard(){
 
 	var board = document.getElementById('game-board'); 
 	
-	for(var i = 0; i < count; i++){
+	for(var i = 0; i < cards.length; i++){
 		var card = document.createElement('div');
 		card.setAttribute("class", "card"); 
+		card.setAttribute('data-card', cards[i]);
+		card.addEventListener('click', isTwoCards);
+
 		board.appendChild(card);
 	}
 }
-createBoard(4);
+createBoard();
+
+function isTwoCards(){
+	var cardImage = this.getAttribute('data-card');
+	cardsInPlay.push(cardImage);
+
+	if(cardImage === "king"){
+		this.innerHTML = '<img src="king.png" alt="King of Clubs" />';	
+	}else{
+		this.innerHTML = '<img src="queen.png" alt="Queen of Hearts" />';	
+	}
+	
+	if(cardsInPlay.length === 2){
+		
+		isMatch(cardsInPlay);
+
+		cardsInPlay = [];
+	}
+}
+
+function isMatch(cardsInPlay){
+
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("Awesome it matches!");
+	}else{
+		alert("Sorry, try again!");
+	}
+
+
+	var cards = document.getElementsByClassName("card");
+
+	for(var i = 0; i < cards.length; i++){
+		cards[i].innerHTML = '';
+	}
+}
